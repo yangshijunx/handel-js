7fc4b2e764a955d0a203a23e90bd26cd2f98ea5b
yangshijun <yangshijun777@gmail.com>
2023-07-10 00:55:35 +0800

    feat: fetchWithRetry

--- fetchWithRetry.js
+++ fetchWithRetry.js
@@ -1,5 +1,26 @@
 import fetch from "node-fetch";
-const fetchWithRetry = async (url, maxRetryTimes) => {
+const fetchWithRetry = async (url, maxRetryTimes = 3) => {
   // console.log("url", url, maxRetryTimes);
+  let retryTimes = 0;
+  return new Promise((resolve, reject) => {
+    const handleFetch = async () => {
+      try {
+        const response = await fetch(url);
+        if(response.status === 200) {
+          resolve(response);
+        } else {
+          throw new Error(response.statusText);
+        }
+      } catch (error) {
+        retryTimes++;
+        if(retryTimes >= maxRetryTimes) {
+          reject(error);
+        } else {
+          handleFetch();
+        }
+      }
+    }
+    handleFetch();
+  });
 }
 export default fetchWithRetry;
